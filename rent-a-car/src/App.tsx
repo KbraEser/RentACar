741dc369daff2ae478f417dee2ab5b9d5f911e5f
KbraEser <kbra.demirbas@gmail.comm>
2025-07-25 13:13:45 +0300

    .gitignore dosyasında gereksiz satırlar kaldırıldı ve yeni ortam değişkenleri eklendi. App bileşeninde CarCard bileşeni kullanıldı ve yeni CarCard bileşeni oluşturuldu.

--- rent-a-car/src/App.tsx
+++ rent-a-car/src/App.tsx
@@ -1,9 +1,5 @@
+import CarCard from "./components/CarCard";
+
 export default function App() {
-  return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Tailwind CSS başarıyla kuruldu!
-      </h1>
-    </div>
-  );
+  return <CarCard />;
 }
