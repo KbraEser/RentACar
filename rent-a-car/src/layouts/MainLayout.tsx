81223ba6590ae4b716dd66f4a776e92544afe0f2
KbraEser <kbra.demirbas@gmail.comm>
2025-09-29 15:31:16 +0300

    authService dosyasına email alanı eklendi.

--- rent-a-car/src/layouts/MainLayout.tsx
+++ rent-a-car/src/layouts/MainLayout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/common/Navbar";
-import Footer from "../components/common/Footer";
 
 const MainLayout = () => {
   return (
